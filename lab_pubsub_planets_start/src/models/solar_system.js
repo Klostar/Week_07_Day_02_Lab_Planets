74c7947ee9c4cd5f7924b3341d4df8f85b951292
Laura Nagy <ngylaura@gmail.com>
2018-09-11 15:46:28 +0100

    adds solar_system model

--- lab_pubsub_planets_start/src/models/solar_system.js
+++ lab_pubsub_planets_start/src/models/solar_system.js
@@ -1,5 +1,19 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const SolarSystem = function(planets) {
   this.planets = planets;
+};
+
+SolarSystem.prototype.bindEvents = function () {
+  PubSub.publish('SolarSystem:planets-ready', this.planets);
+  PubSub.subscribe('PlanetInput:planet-selected', (event) => {
+    const result = this.planets.find( function (planet) {
+      return planet.name == event.detail;
+
+    });
+    console.log(result);
+    PubSub.publish('SolarSystem:return-planet', result);
+  });
 };
 
 module.exports = SolarSystem;
