6df83134fb5ff950a4b1a0e57fce808e4c4cb970
Laura Nagy <ngylaura@gmail.com>
2018-09-11 15:47:12 +0100

    instantiates models and views

--- lab_pubsub_planets_start/src/app.js
+++ lab_pubsub_planets_start/src/app.js
@@ -1,7 +1,16 @@
 const planetsData = require('./data/planets.js');
 const SolarSystem = require('./models/solar_system.js');
+const PlanetInput = require('./views/planetInput.js');
+const PlanetResult = require('./views/planetResult.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const planetsDataModel = new SolarSystem(planetsData);
-  console.log(planetsDataModel.planets);
+
+  const planetResult = new PlanetResult();
+  planetResult.bindEvents();
+
+  const planetInput = new PlanetInput();
+  planetInput.bindEvents();
+
+  planetsDataModel.bindEvents();
 });
